2d672a1f3e359d75babcc5b66fa106dfd2ce8b8f
Maxime <limax84@gmail.com>
2014-08-27 01:16:09 +0200

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+    res.send('respond with a resource');
+});
+
+/* GET users listing. */
+router.get('/test', function(req, res) {
+    res.send('respond with a resource test');
+});
+
+module.exports = router;
