5110b0e089dc0ab445842ee72433c4ce5740f296
Maxime <limax84@gmail.com>
2014-08-27 19:36:24 +0200

    Adapt to the new body-parser "urlencoded" version

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(logger('dev'));
 
 // Setup the JSON and encoded URL parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Setup the cookie parser
 app.use(cookieParser());
